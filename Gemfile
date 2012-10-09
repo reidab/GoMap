d9760c37144a8aea31551c83c495353cb40fdc63
Reid Beels <mail@reidbeels.com>
2012-10-08 20:11:09 -0700

    Support jRuby for ruby-processing

--- Gemfile
+++ Gemfile
@@ -1,8 +1,16 @@
 # A sample Gemfile
 source "https://rubygems.org"
 
-gem 'pg'
 gem 'standalone_migrations'
 gem 'activerecord'
 gem 'httparty'
 
+platform :ruby do
+  gem 'pg'
+end
+
+platform :jruby do
+  gem 'ruby-processing'
+  gem 'jruby-openssl'
+  gem 'activerecord-jdbcpostgresql-adapter'
+end
--- environment.rb
+++ environment.rb
@@ -5,8 +5,10 @@ Bundler.require
 ROOT_PATH = File.dirname(__FILE__)
 
 dbconfig = YAML::load(File.open(File.join(ROOT_PATH, 'db/config.yml')))
-ActiveRecord::Base.establish_connection(dbconfig['development'])
+db_env = (RUBY_PLATFORM == "java") ? 'development_jruby' : 'development'
+
 ActiveRecord::Base.default_timezone = :utc
+ActiveRecord::Base.establish_connection(dbconfig[db_env])
 
 Dir.glob(File.join(ROOT_PATH, 'lib','go_map','*.rb')).each {|rb|
   require rb
